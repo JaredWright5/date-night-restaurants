021ffd9edf165e560e5febc69aff9e4d6b06e9b5
Jared Wright <jaredwright@jareds-mbp.lan>
2025-10-16 21:04:56 -0700

    Fix sitemap.xml routing issue by creating custom sitemap endpoint that serves proper XML content

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ site }) => {
+  // Read the generated sitemap-index.xml
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://datenightrestaurants.com/sitemap-0.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(sitemapIndex, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+};
