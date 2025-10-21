da8ec00641a37056f94a2cb6a1f032c32e550007
Jared Wright <jaredwright@jareds-mbp.lan>
2025-10-20 20:43:12 -0700

    Add default featured restaurants fallback to fix photo loading issues

---
+++ set-featured.js
@@ -0,0 +1,1 @@
+localStorage.setItem("featuredRestaurants", JSON.stringify(["felix-trattoria", "gjelina-on-abbot-kinney", "fishing-with-dynamite", "bavel"])); console.log("Featured restaurants set");
