4e5c8f5094daa1b48e48dd911c5aa0ba09247227
Jared Wright <jaredwright@jareds-mbp.lan>
2025-10-20 20:45:16 -0700

    Add additional debugging to featured section and clean up test files

--- set-featured.js
+++
@@ -1,1 +0,0 @@
-localStorage.setItem("featuredRestaurants", JSON.stringify(["felix-trattoria", "gjelina-on-abbot-kinney", "fishing-with-dynamite", "bavel"])); console.log("Featured restaurants set");
